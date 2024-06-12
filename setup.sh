9e3d9eacc2f7b6678689e1fd2eb01c9baf2457c1
Anirudh Kailaje <kailaje@seas.upenn.edu>
2024-06-11 21:21:58 -0400

    Modified setup.sh

--- setup.sh
+++ setup.sh
@@ -1,34 +1,38 @@
-sudo apt  update && sudo apt -y upgrade
+sudo apt update 
 sudo apt install -y nala
-cd ~/
-mkdir -p ~/.config
+sudo nala update && sudo nala upgrade
+
+username=$(id -u -n 1000)
+builddir=$(pwd)
+
+cd $builddir
+
+mkdir -p /home/$username/.config
+mkdir -p /home/$username/.tmux
+
+cp -R .config/* /home/$username/.config/
+cp -R .tmux/* /home/$username/.tmux/
+sudo mv  Mangalore.jpg /usr/share/backgrounds/
 
 sudo nala install -y  feh kitty i3 picom unzip wget blueman curl
 sudo nala install -y neofetch autorandr light fonts-font-awesome -y
 sudo nala install -y vlc simplescreenrecorder alacritty
 
 sudo nala install software-properties-common
-sudo add-apt-repository ppa:appimagelauncher-team/stable
-sudo nala update
-sudo nala install appimagelauncher 
+sudo mv config /home$username/.config/i3/
 
-sudo apt-get install wget gpg
-wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
-sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
-echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" |sudo tee /etc/apt/sources.list.d/vscode.list > /dev/null
-rm -f packages.microsoft.gpg
+sudo apt-get install  gpg
+sudo nala install -y flatpak filelight gnome-software-plugin-flatpak
+sudo nala install -y plasma-discover-backend-flatpak
 
-sudo apt install -y apt-transport-https
-sudo apt update
-sudo apt install -y code # or code-insiders
-
-sudo nala install -y flathub filelight
+flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 
 sudo flatpak install -y flathub com.discordapp.Discord
 sudo flatpak install -y flathub com.slack.Slack
 sudo flatpak install -y flathub com.tencent.WeChat
 sudo flatpak install -y flathub com.brave.Browser
-
+sudo flatpak install -y flathub com.logseq.Logseq
+sudo flatpak install -y flathub com.visualstudio.code
 git clone https://github.com/AdnanHodzic/auto-cpufreq.git
 cd auto-cpufreq && sudo ./auto-cpufreq-installer
 
